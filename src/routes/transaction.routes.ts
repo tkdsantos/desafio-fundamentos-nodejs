344a15d67304daf2d6778f8bd707850848ec57cb
Daniel Santos <tkdsantos@gmail.com>
2020-05-10 16:54:57 -0300

    implementação do código para adicionar entradas e saídas

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +21,19 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createAppointment = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(appointment);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
